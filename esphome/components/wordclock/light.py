15d18e76e02cfe9a9bdc02c540f4a926d0874397
DirkBilland <mail@dirkbilland.de>
2025-06-02 10:58:16 +0200

    Adaption to wordclock

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
-from esphome.components import light, output
+from esphome.components import rgb, output
 import esphome.config_validation as cv
 from esphome.const import CONF_BLUE, CONF_GREEN, CONF_OUTPUT_ID, CONF_RED
 
-rgb_ns = cg.esphome_ns.namespace("rgb")
-RGBLightOutput = rgb_ns.class_("RGBLightOutput", light.LightOutput)
+wordclock_ns = cg.esphome_ns.namespace("wordclock")
+WordclockLightOutput = wordclock_ns.class_("WordclockLightOutput", rgb.RGBLightOutput)
 
 CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
     {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(WordclockLightOutput),
         cv.Required(CONF_RED): cv.use_id(output.FloatOutput),
         cv.Required(CONF_GREEN): cv.use_id(output.FloatOutput),
         cv.Required(CONF_BLUE): cv.use_id(output.FloatOutput),
