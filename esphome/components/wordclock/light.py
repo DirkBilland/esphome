17ef172101815fcc6b1d5e866815f5af408d6c27
DirkBilland <mail@dirkbilland.de>
2025-06-04 00:59:52 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -9,12 +9,12 @@ WordClockLight = wordclock_ns.class_('WordClockLight', light.LightOutput)
 CONF_INTERNAL_LIGHT = "internal_light"
 
 CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(WordClockLight),
+    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(WordClockLight),
     cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.AddressableLight),
 })
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    await light.register_light(var, config)
     internal = await cg.get_variable(config[CONF_INTERNAL_LIGHT])
     cg.add(var.set_internal_light(internal))
-    await light.register_light(var, config)
