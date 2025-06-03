5db81ca3c028bf9788f7fa000251e27a2cb170d4
DirkBilland <mail@dirkbilland.de>
2025-06-04 00:24:14 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -10,7 +10,7 @@ CONF_INTERNAL_LIGHT = "internal_light"
 
 CONFIG_SCHEMA = light.BINARY_LIGHT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(WordClockLight),
-    cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.AddressableLight),
+    cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.NeoPixelBusLightOutputBase),
 })
 
 async def to_code(config):
