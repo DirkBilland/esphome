948f0c9271a40948d1df60e0f628f31d60db0cc2
DirkBilland <mail@dirkbilland.de>
2025-06-04 01:27:09 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -1,5 +1,5 @@
 import esphome.codegen as cg
-from esphome.components import light, display
+from esphome.components import light, display, addressable_light
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_OUTPUT_ID
 
