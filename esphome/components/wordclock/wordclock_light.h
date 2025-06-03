ca4cdad991af05d5b91784d5413aca34d463513e
DirkBilland <mail@dirkbilland.de>
2025-06-03 22:49:18 +0200

    Create wordclock_light.h

---
+++ esphome/components/wordclock/wordclock_light.h
@@ -0,0 +1,29 @@
+#include "esphome.h"
+
+class WordClockLight : public esphome::light::LightOutput {
+ public:
+  void set_internal_light(esphome::light::AddressableLight* light) {
+    this->internal_light_ = light;
+  }
+
+  esphome::light::LightTraits get_traits() override {
+    auto traits = esphome::light::LightTraits();
+    traits.set_supports_brightness(true);
+    traits.set_supports_rgb(true);
+    return traits;
+  }
+
+  void write_state(esphome::light::LightState* state) override {
+    float r, g, b;
+    state->current_values_as_rgb(&r, &g, &b);
+
+    // Beispiel: LEDs 0–5 für „ES IST“
+    esphome::Color color = esphome::Color::from_rgb(r * 255, g * 255, b * 255);
+    this->internal_light_->all().fade_to_black(); // Alles aus
+    this->internal_light_->range(0, 5) = color;
+    this->internal_light_->schedule_show();
+  }
+
+ protected:
+  esphome::light::AddressableLight* internal_light_;
+};
