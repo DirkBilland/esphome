38680d7bf116ddad829d0bb2793200cd7fee8f25
DirkBilland <mail@dirkbilland.de>
2025-06-04 08:48:05 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -4,7 +4,7 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_OUTPUT_ID
 
 wordclock_ns = cg.esphome_ns.namespace('wordclock')
-WordClockLight = wordclock_ns.class_('WordClockLight', light.AddressableLight)
+WordClockLight = wordclock_ns.class_('WordClockLight', light.LightOutput)
 
 CONF_INTERNAL_LIGHT = "internal_light"
 
