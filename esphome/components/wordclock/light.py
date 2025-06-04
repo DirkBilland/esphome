d2c20b9773d938865395b13fbfd798d802335184
DirkBilland <mail@dirkbilland.de>
2025-06-04 10:49:56 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -6,6 +6,7 @@ from esphome.const import CONF_ID, CONF_OUTPUT_ID
 wordclock_ns = cg.esphome_ns.namespace('wordclock')
 WordClockLight = wordclock_ns.class_('WordClockLight', light.LightOutput)
 
+light_ns = cg.esphome_ns.namespace("light")
 LightOutput = light_ns.class_("LightOutput")
 
 CONF_INTERNAL_LIGHT = "internal_light"
