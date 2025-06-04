8af1dbb3fb6694e5bece79e3fc91816226675c04
DirkBilland <mail@dirkbilland.de>
2025-06-04 23:42:30 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -15,6 +15,6 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await light.register_light(var, config)
+    await cg.register_component(var, config)
     internal = await cg.get_variable(config[CONF_INTERNAL_LIGHT])
     cg.add(var.set_internal_light(internal))
