9b2dea6c2d7138a4f060691ee986b3d66f718629
DirkBilland <mail@dirkbilland.de>
2025-06-04 23:09:26 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -8,8 +8,8 @@ WordClockLight = wordclocklight_ns.class_('WordClockLight', light.LightOutput)
 
 CONF_INTERNAL_LIGHT = "internal_light"
 
-CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(WordClockLight),
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(WordClockLight),
     cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.AddressableLight),
 })
 
