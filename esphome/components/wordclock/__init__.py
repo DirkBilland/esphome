0e3b36d670c16342e64049de42322a439b7f579a
DirkBilland <mail@dirkbilland.de>
2025-06-05 22:46:10 +0200

    Update __init__.py

--- esphome/components/wordclock/__init__.py
+++ esphome/components/wordclock/__init__.py
@@ -10,7 +10,7 @@ CONF_INTERNAL_LIGHT = "internal_light"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(WordClock),
-    cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(AddressableLightOutput),
+    cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.AddressableLight),
 })
 
 async def to_code(config):
