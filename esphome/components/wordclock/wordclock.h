1503a8c6f90183421b4c13a4dd5ce403788463f8
DirkBilland <mail@dirkbilland.de>
2025-06-05 11:01:55 +0200

    Update wordclock.h

--- esphome/components/wordclock/wordclock.h
+++ esphome/components/wordclock/wordclock.h
@@ -2,8 +2,8 @@
 
 #include "esphome/core/component.h"
 
-using namespace esphome
-using namespace light
+using namespace esphome;
+using namespace light;
 
 namespace esphome {
 namespace wordclock {
