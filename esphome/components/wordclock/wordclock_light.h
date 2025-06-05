3c176e82c11e69c4851fc6f04698d43e5d2f03c2
DirkBilland <mail@dirkbilland.de>
2025-06-05 17:34:36 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -10,6 +10,44 @@ namespace wordclock {
 
 static const char *TAG = "example_component.component";
 
+int leds_time_it_is[] = {20, -1, -1, -1, -1}; // ES IST
+int leds_minutes[] = {124, 123, 122, 121}; // Minutes LEDS
+int leds_skiped[] = {110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120}; // Check Line 101
+int leds_time_minutes[][3] = {
+    {  4,  -1,  -1}, // UHR
+    { 21,  17,  -1}, // FÜNF, NACH
+    { 22,  17,  -1}, // ZEHN, NACH
+    { 23,  17,  -1}, // VIERTEL, NACH
+    { 24,  17,  -1}, // ZWANZIG, NACH
+    { 21,  18,  16}, // FÜNF, VOR, HALB
+    { 16,  -1,  -1}, // HALB
+    { 21,  17,  16}, // FÜNF, NACH, HALB
+    { 24,  18,  -1}, // ZWANZIG, VOR
+    { 19,  -1,  -1}, // DREIVIERTEL
+    { 22,  18,  -1}, // ZEHN, VOR
+    { 21,  18,  -1}  // FÜNF, VOR
+};
+
+int leds_time_hours[][6] = {
+    {  3,  -1,  -1,  -1,  -1,  -1}, // ZWÖLF
+    { 12,  -1,  -1,  -1,  -1,  -1}, // EINS
+    { 13,  -1,  -1,  -1,  -1,  -1}, // ZWEI
+    { 14,  -1,  -1,  -1,  -1,  -1}, // DREI
+    {  9,  -1,  -1,  -1,  -1,  -1}, // VIER
+    {  8,  -1,  -1,  -1,  -1,  -1}, // FÜNF
+    {  7,  -1,  -1,  -1,  -1,  -1}, // SECHS
+    {  6,  -1,  -1,  -1,  -1,  -1}, // SIEBEN
+    {  5,  -1,  -1,  -1,  -1,  -1}, // ACHT
+    {  0,  -1,  -1,  -1,  -1,  -1}, // NEUN
+    {  1,  -1,  -1,  -1,  -1,  -1}, // ZEHN
+    {  2,  -1,  -1,  -1,  -1,  -1}, // ELF
+    { 11,  -1,  -1,  -1,  -1,  -1}  // EIN
+};
+
+int hour = -1;
+int minute = -1;
+int second = -1;
+
 bool change = false;
 
 class WordClock : public Component, public LightOutput {
@@ -44,14 +82,80 @@ class WordClock : public Component, public LightOutput {
       ESP_LOGD("write_state", "New color %i %i %i", red, green, blue );
   }
 
-void setup() {
-    // Code here should perform all component initialization,
-    //  whether hardware, memory, or otherwise
+  void setup() override {
+    strip.Begin();
+    strip.ClearTo(RgbColor(0, 0, 0));
+    strip.Show();
+  
+    // // Start all LED with on and default color and brightness to check if everything is working...
+    for(int i = 0; i < PixelCount; i++) { strip.SetPixelColor(i, RgbColor(red, 0, 0)); strip.Show(); delay(10); }
+    for(int i = 0; i < PixelCount; i++) { strip.SetPixelColor(i, RgbColor(0, green, 0)); strip.Show(); delay(10); }
+    for(int i = 0; i < PixelCount; i++) { strip.SetPixelColor(i, RgbColor(0, 0, blue)); strip.Show(); delay(10); }
+    strip.ClearTo(RgbColor(0, 0, 0));
+    strip.Show();
+    ESP_LOGD("setup", "Setup done!");
+   //register_service(&Wordclock::on_setled, "setled", {"number","red", "blue", "green"});
   }
   
-  void loop() {
-    // Tasks here will be performed at every call of the main application loop.
-    // Note: code here MUST NOT BLOCK (see below)
+  void loop() override {
+      //auto time = id(current_time).now();
+      auto time = ESPTime::from_epoch_local( id(current_time).timestamp_now() + 150);
+      int h = time.hour;
+      int m = time.minute;
+      int s = time.second;
+
+      boxclocklight->current_values_as_rgb(&fred, &fgreen, &fblue);
+      //convert float 0.0 till 1.0 into int 0 till 255
+      red = fred * 255;
+      green = fgreen * 255;
+      blue = fblue * 255;
+
+      //auto ledlight = id(boxclocklight).remote_values;
+      // https://www.esphome.io/api/classesphome_1_1light_1_1_light_color_values.html LightColorValues Class
+
+      // red = (int)(ledlight.get_red()*255);
+      // green = (int)(ledlight.get_green()*255);
+      // blue = (int)(ledlight.get_blue()*255);
+
+
+       //check if valid time. Blink red,green,blue until valid time is present
+      if (time.is_valid() == false) {
+          ESP_LOGE("loop", "Got invalid time from current_time Time: %i:%i", h, m );
+          strip.SetPixelColor(0, RgbColor(255, 0, 0)); strip.Show(); delay(250);
+          strip.SetPixelColor(0, RgbColor(0, 255, 0)); strip.Show(); delay(250);
+          strip.SetPixelColor(0, RgbColor(0, 0, 255)); strip.Show(); delay(250);
+          strip.SetPixelColor(0, RgbColor(0, 0, 0));   strip.Show();
+    }
+      else {
+          // only update once in a Minute
+//                if(h != hour || m != minute) {
+          if ((s != second) || change) {
+              ESP_LOGD("loop", "Using b: %i rgb %i %i %i", brightness, red, green, blue);
+              hour = h;
+              minute = m;  
+              second = s;
+              change = 0;
+              if (hour >= 0 && time.is_valid() == true){
+                  int tmp_hour = hour;
+                  int tmp_minute = (minute - (minute % 5));
+                  if(tmp_minute >= 25) { tmp_hour += 1; }
+                  tmp_minute = tmp_minute / 5;
+                  tmp_hour = tmp_hour % 12;
+                  if ((tmp_hour == 1) && (tmp_minute == 0))  //"EINS" anstelle von "EIN" verwenden
+                      tmp_hour += 11;  
+                  int minutessum = minute % 5;
+                  // Reset all LED, but skip LED 110 till 120
+                  for(int i = 0; i < PixelCount; i++) {     if(i < 110 || i > 120) strip.SetPixelColor(i, RgbColor(0, 0, 0)); }
+                  for(int i = 0; i < 5; i++) {            strip.SetPixelColor(leds_time_it_is[i], RgbColor(red, green, blue)); }
+                  for(int i = 0; i < 3; i++) {           if(leds_time_minutes[tmp_minute][i] >= 0) { strip.SetPixelColor(leds_time_minutes[tmp_minute][i], RgbColor(red, green, blue)); } }
+                  for(int i = 0; i < 6; i++) {            if(leds_time_hours[tmp_hour][i] >= 0) { strip.SetPixelColor(leds_time_hours[tmp_hour][i], RgbColor(red, green, blue)); } }
+//                        for(int i = 0; i < minutessum; i++) {   leds[leds_minutes[i]].setRGB(red, green, blue);}
+                  strip.Show();
+                  ESP_LOGD("loop", "Update Time: %i:%i  Brightness: %i RGB: %i-%i-%i", hour, minute, brightness, red, green, blue);
+                  ESP_LOGD("loop", "Using tmp_hour: %i tmp_minute: %i minutessum: %i", tmp_hour, tmp_minute, minutessum);
+              }
+          }
+      }
   }
   
   void dump_config(){
