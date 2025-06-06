01394bf6b9d509d7891af9ad363904b2421d7e1a
DirkBilland <mail@dirkbilland.de>
2025-06-06 22:21:11 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -131,7 +131,7 @@ class WordClock : public Component, public LightOutput {
       if (time.is_valid() == false) {
           ESP_LOGE("loop", "Got invalid time from current_time Time: %i:%i", h, m );
           //it[0] = Color(255, 0, 0); delay(250);
-          internal_light_[0] = Color(255, 0, 0); delay(250);
+          internal_light_->get_controller()->leds()[0].setRGB(r, g, b);
           strip.SetPixelColor(0, RgbColor(255, 0, 0)); strip.Show(); delay(250);
           strip.SetPixelColor(0, RgbColor(0, 255, 0)); strip.Show(); delay(250);
           strip.SetPixelColor(0, RgbColor(0, 0, 255)); strip.Show(); delay(250);
