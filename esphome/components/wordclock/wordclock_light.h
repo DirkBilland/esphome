7bc9603a75cbf180e05ff295fba997b97f19473f
DirkBilland <mail@dirkbilland.de>
2025-06-10 22:51:22 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -108,9 +108,9 @@ class WordClock : public Component, public LightOutput {
   void loop() override {
       //auto time = id(current_time).now();
 //      auto time = ESPTime::from_epoch_local( current_time->timestamp_now() + 150);
-      int h = time.hour;
-      int m = time.minute;
-      int s = time.second;
+      // int h = time.hour;
+      // int m = time.minute;
+      // int s = time.second;
 
 //      boxclocklight->current_values_as_rgb(&fred, &fgreen, &fblue);
       //convert float 0.0 till 1.0 into int 0 till 255
