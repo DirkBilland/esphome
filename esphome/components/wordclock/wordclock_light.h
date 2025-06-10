8255ee92f5e3ba7bcc6178a4a9e2bf57e831fce9
DirkBilland <mail@dirkbilland.de>
2025-06-10 23:00:23 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -84,10 +84,11 @@ class WordClock : public Component, public LightOutput {
 
   void setup() override {
     internal_light_->all() = Color::BLACK;
-    internal_light_->all() = Color(red, 0, 0); delay(10);
-    internal_light_->all() = Color(0, green, 0); delay(10);
-    internal_light_->all() = Color(0, 0, blue); delay(10);
+    internal_light_->all() = Color(red, 0, 0); internal_light_->update_state(); delay(10);
+    internal_light_->all() = Color(0, green, 0); internal_light_->update_state(); delay(10);
+    internal_light_->all() = Color(0, 0, blue); internal_light_->update_state(); delay(10);
     internal_light_->all() = Color::BLACK;
+    internal_light_->update_state();
 
     ESP_LOGD("setup", "Setup done!");
     
