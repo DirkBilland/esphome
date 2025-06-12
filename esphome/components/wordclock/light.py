0b46c1093fa76d9bf74fa7d0654af566f62e740e
DirkBilland <mail@dirkbilland.de>
2025-06-12 21:04:14 +0200

    Update light.py

--- esphome/components/wordclock/light.py
+++ esphome/components/wordclock/light.py
@@ -1,8 +1,8 @@
 
 import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.components import light, output
 from esphome.components import neopixelbus, time as time_
-import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_OUTPUT_ID
 
 wordclock_light_ns = cg.esphome_ns.namespace("wordclock_light")
@@ -13,14 +13,16 @@ CONF_TIMESOURCE = "timesource"
 
 CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend(
     {
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(WordClockLight),
         cv.Required(CONF_INTERNAL_LIGHT): cv.use_id(light.AddressableLight),
         cv.Required(CONF_TIMESOURCE): cv.use_id(time_.RealTimeClock )
     }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await light.register_light(var, config)
+    
     internal = await cg.get_variable(config[CONF_INTERNAL_LIGHT])
     cg.add(var.set_internal_light(internal))
     internal = await cg.get_variable(config[CONF_TIMESOURCE])
