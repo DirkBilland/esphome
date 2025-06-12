c37fd9cec468261f1b0929609e3d02569f44de35
DirkBilland <mail@dirkbilland.de>
2025-06-12 22:33:08 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -94,7 +94,7 @@ class WordClockLight : public light::LightOutput, public Component {
     internal_light_->all().set(Color(red, 0, 0)); internal_light_->schedule_show(); 
     internal_light_->all().set(Color(0, green, 0)); internal_light_->schedule_show(); 
     internal_light_->all().set(Color(0, 0, blue)); internal_light_->schedule_show(); 
-    internal_light_->all()set(Color::BLACK); internal_light_->schedule_show();
+    internal_light_->all().set(Color::BLACK); internal_light_->schedule_show();
     ESP_LOGD("setup", "Setup done!");
     
 //     strip->Begin();
