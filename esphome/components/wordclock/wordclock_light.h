5810f2fd0a63eca701ceb728b65bc23c1228bcba
DirkBilland <mail@dirkbilland.de>
2025-06-12 21:49:55 +0200

    Update wordclock_light.h

--- esphome/components/wordclock/wordclock_light.h
+++ esphome/components/wordclock/wordclock_light.h
@@ -120,7 +120,7 @@ class WordClockLight : public light::LightOutput, public Component {
       int m = time.minute;
       int s = time.second;
 
-      this->current_values_as_rgb(&fred, &fgreen, &fblue);
+      internal_light_->current_values_as_rgb(&fred, &fgreen, &fblue);
       //convert float 0.0 till 1.0 into int 0 till 255
       red = fred * 255;
       green = fgreen * 255;
